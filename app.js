3363118c64abbf162f0fc2f58ca80c95d1bdf43b
Fabricio Augusto Oliveira Bastos <168025112+Gutobastos@users.noreply.github.com>
2025-01-22 16:56:38 -0300

    app.js

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ let numeroDaLista = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-// function exibirTextoNaTela(tag, texto) {
-//     let campo = document.querySelector(tag);
-//     campo.innerHTML = texto;
-//     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
-// }
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    if ('speechSynthesis' in window) {
-        let utterance = new SpeechSynthesisUtterance(texto);
-        utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.2; 
-        window.speechSynthesis.speak(utterance); 
-    } else {
-        console.log("Web Speech API não suportada neste navegador.");
-    }
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
 }
+// function exibirTextoNaTela(tag, texto) {
+//     let campo = document.querySelector(tag);
+//     campo.innerHTML = texto;
+//     if ('speechSynthesis' in window) {
+//         let utterance = new SpeechSynthesisUtterance(texto);
+//         utterance.lang = 'pt-BR'; 
+//         utterance.rate = 1.2; 
+//         window.speechSynthesis.speak(utterance); 
+//     } else {
+//         console.log("Web Speech API não suportada neste navegador.");
+//     }
+// }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
